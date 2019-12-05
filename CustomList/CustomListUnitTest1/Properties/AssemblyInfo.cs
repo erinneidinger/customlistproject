8b4abb656d38b572bc3b300e1b8f9b60d844fa0e
Erinn Neidinger <erinndneidinger@gmail.com>
2019-12-05 11:01:49 -0600

    Created repo for custom list project.

---
+++ CustomList/CustomListUnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListUnitTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("33866542-0c1d-4797-b1ce-0e7a990c9358")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomList/CustomListUnitTest1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListUnitTest1
+{
+    [TestClass]
+    public class CustomList
+    {
+        [TestMethod]
+        public void Addition_SumEqualsExpected_ReturnCorrectSum()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}
